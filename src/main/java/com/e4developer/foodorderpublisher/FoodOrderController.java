95b9ed53a0fc40bc548ddf6e91497f8af90e86e2
Bartosz Jedrzejewski <bjedrzejewski@scottlogic.com>
2018-02-06 23:11:38 +0000

    zipkin-ready

--- src/main/java/com/e4developer/foodorderpublisher/FoodOrderController.java
+++ src/main/java/com/e4developer/foodorderpublisher/FoodOrderController.java
@@ -1,6 +1,7 @@
 package com.e4developer.foodorderpublisher;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,13 @@ public class FoodOrderController {
     @Autowired
     FoodOrderSource foodOrderSource;
 
+    @Autowired
+    Tracer tracer;
+
     @RequestMapping("/order")
     @ResponseBody
     public String orderFood(@RequestBody FoodOrder foodOrder){
-        foodOrderSource.foodOrders().send(MessageBuilder.withPayload(foodOrder).build());
+        foodOrderSource.foodOrders().send(MessageBuilder.withPayload(foodOrder).setCorrelationId(tracer.getCurrentSpan().getTraceId()).build());
         System.out.println(foodOrder.toString());
         return "food ordered!";
     }
